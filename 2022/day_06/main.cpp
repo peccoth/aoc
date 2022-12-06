2b0becec21d90d844a5376bd7779832e0afc5a00
peccoth <peccoth101@gmail.com>
2022-12-06 14:14:40 +0100

    day 6 no set

--- 2022/day_06/main.cpp
+++ 2022/day_06/main.cpp
@@ -1,23 +1,25 @@
 #include<iostream>
-#include<unordered_set>
-int solve(std::string &in, int size) {
+#include<stdlib.h>
+int solve(char *in, int size) {
     int result = 0;
-    std::unordered_set<char> set;
-    for (result=0; result < in.length(); result++){
+    for (result=0; in[result + size] != '\0'; result++){
+        bool found[26] = { 0 };
+        int success = 0;
         for (int i = 0; i < size; i++)
-            set.insert(in[result + i]);
-
-        if (set.size() == size)
+            found[in[result + i] - 'a'] = 1;
+        for (int i = 0; i < 26; i++)
+            success += found[i];
+        if (success > size - 1)
             return result + size;
-        else 
-            set.clear();
     }
-    return 0;
+    return -1;
 }
 
 int main() {
-    std::string str;
+    char * str = (char*)calloc(100000001, sizeof(char));
     std::cin>>str;
+    str[100000000] = '\0';
+
     int silver = solve(str,4);
     int gold = solve(str,14);
 
